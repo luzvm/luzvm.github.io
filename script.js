075fa0cbcac0d8d0f03e0750865ccf31d27e334b
luzvm <135098366+luzvm@users.noreply.github.com>
2023-05-31 07:13:23 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,54 +1,40 @@
-async function predictImage(imageElement) {
+// Cargar el modelo
+async function loadModel() {
   const model = await tf.loadLayersModel('model.json');
-  const metadata = {
-    tfjsVersion: "1.3.1",
-    tmVersion: "2.4.5",
-    packageVersion: "0.8.4-alpha2",
-    packageName: "@teachablemachine/image",
-    timeStamp: "2023-05-31T04:07:27.234Z",
-    userMetadata: {},
-    modelName: "tm-my-image-model",
-    labels: ["Folding marks", "Grain off", "Growth marks", "loose grains", "non defective", "pinhole"],
-    imageSize: 224
-  };
-
-  const image = tf.browser.fromPixels(imageElement).toFloat().expandDims();
-  const normalizedImage = image.div(255.0);
-
-  const predictions = await model.predict(normalizedImage).data();
-
-  const classes = metadata.labels;
-  const results = Array.from(predictions).map((prediction, index) => ({
-    className: classes[index],
-    probability: prediction.toFixed(4)
-  }));
-
-  return results;
+  return model;
 }
 
+// Preprocesar la imagen
+function preprocessImage(image) {
+  const resizedImage = tf.image.resizeBilinear(image, [256, 256]);
+  const normalizedImage = resizedImage.div(255.0);
+  const batchedImage = normalizedImage.expandDims(0);
+  return batchedImage;
+}
+
+// Realizar la predicción de la imagen
+async function predictImage(image) {
+  const model = await loadModel();
+  const processedImage = preprocessImage(image);
+  const predictions = await model.predict(processedImage).data();
+  return predictions;
+}
+
+// Actualizar los campos de resultados en la página
 function updateResultFields(results) {
-  document.getElementById('pliegues').textContent = results[0].probability;
-  document.getElementById('class2').textContent = results[1].probability;
-  document.getElementById('class3').textContent = results[2].probability;
-  document.getElementById('class4').textContent = results[3].probability;
-  document.getElementById('class5').textContent = results[4].probability;
-  document.getElementById('class6').textContent = results[5].probability;
+  const labels = ["Folding marks", "Grain off", "Growth marks", "loose grains", "non defective", "pinhole"];
+  for (let i = 0; i < results.length; i++) {
+    const probability = results[i].toFixed(4);
+    document.getElementById(`class${i + 1}`).textContent = probability;
+  }
 }
 
-document.getElementById("upload-pc-button").addEventListener("click", function() {
-  document.getElementById("upload-pc").click();
-});
-
-document.getElementById("upload-pc").addEventListener("change", handleImageUpload);
-document.getElementById("upload-button-url").addEventListener("click", loadImageFromURL);
-
+// Manejar la carga de imágenes desde el archivo seleccionado
 function handleImageUpload(event) {
   const imageFile = event.target.files[0];
   const imagePreview = document.getElementById("image-preview");
 
   if (imageFile) {
-    showImagePreview(event);
-
     const imageElement = new Image();
     imageElement.onload = async () => {
       const results = await predictImage(imageElement);
@@ -61,29 +47,12 @@ function handleImageUpload(event) {
   }
 }
 
-function showImagePreview(event) {
-  var preview = document.getElementById("image-preview");
-  var file = event.target.files[0];
-  var reader = new FileReader();
-
-  reader.onloadend = function () {
-    preview.src = reader.result;
-  }
-
-  if (file) {
-    reader.readAsDataURL(file);
-  } else {
-    preview.src = "";
-  }
-}
-
+// Manejar la carga de imágenes desde una URL
 function loadImageFromURL() {
-  var imageUrl = document.getElementById("image-url").value;
-  var imagePreview = document.getElementById("image-preview");
+  const imageUrl = document.getElementById("image-url").value;
+  const imagePreview = document.getElementById("image-preview");
 
   if (imageUrl) {
-    showImagePreviewURL(imageUrl);
-
     const imageElement = new Image();
     imageElement.crossOrigin = "anonymous";
     imageElement.onload = async () => {
@@ -97,7 +66,27 @@ function loadImageFromURL() {
   }
 }
 
-function showImagePreviewURL(url) {
-  var preview = document.getElementById("image-preview");
-  preview.src = url;
+// Mostrar una vista previa de la imagen cargada
+function showImagePreview(event) {
+  const preview = document.getElementById("image-preview");
+  const file = event.target.files[0];
+  const reader = new FileReader();
+
+  reader.onloadend = function() {
+    preview.src = reader.result;
+  };
+
+  if (file) {
+    reader.readAsDataURL(file);
+  } else {
+    preview.src = "";
+  }
 }
+
+// Asociar eventos a los elementos de la interfaz
+document.getElementById("upload-pc-button").addEventListener("click", function() {
+  document.getElementById("upload-pc").click();
+});
+
+document.getElementById("upload-pc").addEventListener("change", handleImageUpload);
+document.getElementById("upload-button-url").addEventListener("click", loadImageFromURL);
